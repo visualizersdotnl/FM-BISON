1156a0fbb720cac2aa5e6664fd5b19fdf2c31216
Niels J. de Wit <ndewit@gmail.com>
2020-06-24 00:57:36 +0200

    Update synth-math-easings.h

--- helper/synth-math-easings.h
+++ helper/synth-math-easings.h
@@ -1,6 +1,6 @@
 
 /*
-	FM. BISON hybrid FM synthesis -- Interpolation functions all copied from: https://easings.net/
+	FM. BISON hybrid FM synthesis -- Interpolation functions all copied from: https://easings.net/ (https://github.com/ai/easings.net/blob/master/src/easings.yml)
 	(C) visualizers.nl & bipolaraudio.nl
 	MIT license applies, please see https://en.wikipedia.org/wiki/MIT_License or LICENSE in the project root!
 
