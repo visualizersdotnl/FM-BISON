a760f4bb89f2ddff02c92641dfef4a369a73e61e
Niels J. de Wit <ndewit@gmail.com>
2020-11-30 01:10:45 +0100

    Update synth-post-pass.cpp

--- synth-post-pass.cpp
+++ synth-post-pass.cpp
@@ -129,7 +129,7 @@ namespace SFM
 	                     float cpRate, float cpWet, bool isChorus,
 	                     float delayInSec, float delayWet, float delayDrivedB, float delayFeedback, float delayFeedbackCutoff, float delayTapeWow,
 	                     float postCutoff, float postReso, float postDrivedB, float postWet,
-						 float tubeDistort, float tubeDrive, float tubeOffset,
+			     float tubeDistort, float tubeDrive, float tubeOffset,
 	                     float reverbWet, float reverbRoomSize, float reverbDampening, float reverbWidth, float reverbLP, float reverbHP, float reverbPreDelay,
 	                     float compThresholddB, float compKneedB, float compRatio, float compGaindB, float compAttack, float compRelease, float compLookahead, bool compAutoGain, float compRMSToPeak,
 	                     float bassTuningdB, float trebleTuningdB, float midTuningdB, float masterVoldB,
