6b3f47fc2bac631fdf0ebd278cd73290a014f60d
Niels J. de Wit <ndewit@gmail.com>
2021-11-24 19:26:13 +0100

    Lingering stuff...

--- synth-Linkwitz-Riley.cpp
+++ synth-Linkwitz-Riley.cpp
@@ -1,6 +1,6 @@
 	
 /*
-	FM. BISON hybrid FM synthesis -- 4th order Linkwitz-Riley filter.
+	FM. BISON hybrid FM synthesis -- Linkwitz-Riley filter (crossover).
 	(C) visualizers.nl & bipolaraudio.nl
 	MIT license applies, please see https://en.wikipedia.org/wiki/MIT_License or LICENSE in the project root!
 */
@@ -11,4 +11,6 @@
 
 namespace SFM
 {
+	void LinkwitzRiley::Reset() {}
+	void LinkwitzRiley::SetCutoff(float Fc) {}
 }
--- synth-Linkwitz-Riley.h
+++ synth-Linkwitz-Riley.h
@@ -1,8 +1,20 @@
 	
 /*
-	FM. BISON hybrid FM synthesis -- 4th order Linkwitz-Riley filter.
+	FM. BISON hybrid FM synthesis -- Linkwitz-Riley filter (crossover).
 	(C) visualizers.nl & bipolaraudio.nl
 	MIT license applies, please see https://en.wikipedia.org/wiki/MIT_License or LICENSE in the project root!
+
+    What is this 'LR' filter? https://en.wikipedia.org/wiki/Linkwitz%E2%80%93Riley_filter    
+
+    In short it is a filter that's mosten often used for band separation; these filters are widely used as crossovers
+    in for example speakers or mixers.
+
+    A few key properties:
+    - The sum of both the LPF and HPF has a flat (unit) magnitude frequency response curve, ergo, it retains equal power.
+    - The Butterworth filter is ideally suitable to construct this filter as it has zero ripple in the passband and beyond.
+    - As with any 4th-order filter this one has a -24dB per octave (logarithmic) slope.
+
+    FIXME: WIP!
 */
 
 #pragma once
@@ -11,4 +23,20 @@
 
 namespace SFM
 {
+    class LinkwitzRiley
+    {
+    public:
+        LinkwitzRiley(unsigned sampleRate) :
+            m_sampleRate(sampleRate)
+        {
+        }
+
+        ~LinkwitzRiley() {}
+
+        void Reset();
+        void SetCutoff(float Fc);
+
+    private:
+        const unsigned m_sampleRate;
+    };
 }
